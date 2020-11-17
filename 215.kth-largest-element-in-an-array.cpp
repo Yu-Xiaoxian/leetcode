4daa70fb2c8eb96048c5cfe27a2ae547f13214a5
Yu-Xiaoxian <self@yu-xiaoxian.me>
2020-11-17 23:10:13 +0800

    :sparkles: feat(SLAM): add solution to 215 using quick sort

---
+++ 215.kth-largest-element-in-an-array.cpp
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=215 lang=cpp
+ *
+ * [215] Kth Largest Element in an Array
+ */
+
+#include <vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int begin = 0;
+        int end = nums.size() - 1;
+        while(begin < end){
+            int p = partition(nums, begin, end);
+            if(p == k-1){
+                break;
+            } else if(p < k-1){
+                begin = p+1;
+            } else if(p > k-1){
+                end = p-1;
+            }
+        }
+        return nums[k-1];
+    }
+
+    int partition(vector<int>& nums, int begin, int end){
+        int pivot = nums[end];
+        int p = begin;
+        for(int i = begin; i< end; i++){
+            if(nums[i] > pivot){
+                int tmp = nums[i];
+                nums[i] = nums[p];
+                nums[p] = tmp;
+                p++;
+            }
+        }
+        int tmp = nums[p];
+        nums[p] = nums[end];
+        nums[end] = tmp;
+        return p;
+    }
+};
+// @lc code=end
+
+int main(int argc, char** argv){
+    Solution solu;
+    return 0;
+}
+
